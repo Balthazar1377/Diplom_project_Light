72602c0b4538eb70235591905a73f789fa39226c
Balthazar1377 <62011118+Balthazar1377@users.noreply.github.com>
2020-05-16 19:27:21 +0300

    feat: popUpCall is finished

--- src/modules/popUpCall.js
+++ src/modules/popUpCall.js
@@ -1,16 +1,23 @@
 const popUpCall = () => {
-    const popUpCall = document.querySelector('.popup-call');
+    const popUpCall = document.querySelector('.popup-call'),
+        inputs = popUpCall.querySelectorAll('input');
 
     document.body.addEventListener('click', event => {
         const target = event.target;
         if(target.closest('.call-btn')){
            popUpCall.style.display = 'block';
+           inputs.forEach(elem => {
+               elem.toggleAttribute('required');
+           });
         }
     });
     popUpCall.addEventListener('click', event => {
         const target = event.target;
         if(target.closest('.popup-close') || !target.closest('.popup-content')){
             popUpCall.style.display = 'none';
+            inputs.forEach(elem => {
+                elem.toggleAttribute('required');
+            });
         }
     });
 
