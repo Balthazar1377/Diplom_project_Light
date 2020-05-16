743665a8b70510b971bcbc09840a06a0f092acf7
Balthazar1377 <62011118+Balthazar1377@users.noreply.github.com>
2020-05-16 19:21:16 +0300

    feat: finish popUpCall

--- src/modules/popUpCall.js
+++ src/modules/popUpCall.js
@@ -1,6 +1,21 @@
 const popUpCall = () => {
-    
+    const popUpCall = document.querySelector('.popup-call');
+
+    document.body.addEventListener('click', event => {
+        const target = event.target;
+        if(target.closest('.call-btn')){
+           popUpCall.style.display = 'block';
+        }
+    });
+    popUpCall.addEventListener('click', event => {
+        const target = event.target;
+        if(target.closest('.popup-close') || !target.closest('.popup-content')){
+            popUpCall.style.display = 'none';
+        }
+    });
+
 };
 
+//Настроить ошибку при закрытии окна с помощью кнопки крестик
 
 export default popUpCall;
