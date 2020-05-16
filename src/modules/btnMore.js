0d458754adb993914b5a05a37a9b4e46a80f833f
Balthazar1377 <62011118+Balthazar1377@users.noreply.github.com>
2020-05-17 01:48:29 +0300

    feat: finish button and modal windows

--- src/modules/btnMore.js
+++ src/modules/btnMore.js
@@ -1,6 +1,14 @@
 const btnMore = () => {
-
+    const hiddenBlocks = document.querySelectorAll('.hidden'),
+        button = document.querySelector('.add-sentence-btn');
+    document.addEventListener('click', event => {
+        const target = event.target;
+        if(target.closest('.add-sentence-btn')){
+            hiddenBlocks.forEach(elem => {
+                elem.classList.remove('hidden');
+                button.style.display = 'none';
+            });
+        }
+    });
 };
-
-
 export default btnMore;
