a8ed1e06e7644133ec0abbecb4eb3ae330675904
Balthazar1377 <62011118+Balthazar1377@users.noreply.github.com>
2020-05-19 05:28:39 +0300

    feat: finish 60%

--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -1,5 +1,30 @@
 const accordeon = () => {
-
+    const panelGroup = document.getElementById('accordion-two'),
+        panelHeading = panelGroup.querySelectorAll('.panel-heading'),
+        panelCollapse = panelGroup.querySelectorAll('.panel-collapse');
+        console.log(panelGroup);
+        console.log(panelHeading);
+        console.log(panelCollapse);
+    const tab = index => {
+        for(let i = 0; i < panelCollapse.length; i++){
+            if(index === i){
+                panelCollapse[i].classList.add('in');
+            } else {
+                panelCollapse[i].classList.remove('in'); 
+            }
+        }
+    };
+    document.addEventListener('click', event => {
+        let target = event.target;
+        target = target.closest('.panel-heading');
+        if(target){
+            panelHeading.forEach((item, i) => {
+                if(item === target){
+                    tab(i);
+                }
+            });
+        }
+    });
 };
 
 
