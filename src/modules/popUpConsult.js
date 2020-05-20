2f34dac1c7d84c9a1a70624f761c34d3863f2012
Balthazar1377 <62011118+Balthazar1377@users.noreply.github.com>
2020-05-20 06:18:09 +0300

    feat: finish 80%

--- src/modules/popUpConsult.js
+++ src/modules/popUpConsult.js
@@ -1,4 +1,42 @@
 const popUpConsult = () => {
+    const popUpConsult = document.querySelector('.popup-consultation'),
+        inputsConsult = popUpConsult.querySelectorAll('input'),
+        form = popUpConsult.querySelector('form'),
+        directorForm = document.querySelector('.director-form'),
+        inputValue = directorForm.querySelector('input');
 
+    document.addEventListener('click', event => {
+        const target = event.target;
+        if(target.closest('.consultation-btn')){
+            event.preventDefault();
+            popUpConsult.style.display = 'block';
+            inputsConsult.forEach(elem => {
+                elem.toggleAttribute('required');
+            });
+        }
+    });
+    popUpConsult.addEventListener('click', event => {
+        const target = event.target;
+        if(target.closest('.popup-close') || !target.closest('.popup-content')){
+            popUpConsult.style.display = 'none';
+            inputsConsult.forEach(elem => {
+                elem.toggleAttribute('required');
+            });
+        }
+    });
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const request = new XMLHttpRequest();
+        request.open('POST', './server.php');
+        request.setRequestHeader('Content-Type', 'application/json');
+        const formData = new FormData(form);
+        formData.append('question', inputValue.value);
+        let body = {};
+        formData.forEach((value, key) => {
+            body[key] = value;
+        });
+        request.send(JSON.stringify(body));
+    });
+    
 };
 export default popUpConsult;
--- src/modules/sendForms.js
+++ src/modules/sendForms.js
@@ -1,4 +1,75 @@
 const sendForms = () => {
+    const main = document.querySelector('main'),
+        formMain = main.querySelector('form'),
+        inputMain = formMain.querySelector('input');
+    const errorMessage = 'Произошла ошибка при отправке данных на сервер',
+        loadMessage = 'Пожалуйста, подождите...',
+        successMessage = 'Ваши данные были успешно отправлены';
+    const statusMessage = document.createElement('div');
+    inputMain.addEventListener('input', ()=> {
+        inputMain.value = inputMain.value.replace(/[^+\d]/g, '');
+    });
+    formMain.addEventListener('submit', event => {
+        event.preventDefault();
+        formMain.appendChild(statusMessage);
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+            statusMessage.textContent = loadMessage;
+            if(request.readyState !== 4) {
+                return;
+            }
+            if(request.status === 200) {
+                statusMessage.textContent = successMessage;
+            } else {
+                statusMessage.textContent = errorMessage;
+            }
+        });
+        request.open('POST', './server.php');
+        request.setRequestHeader('Content-Type', 'application/json');
+        const formData = new FormData(formMain);
+        let body = {};
+        formData.forEach((value, key) => {
+            body[key] = value;
+        });
+        request.send(JSON.stringify(body));
+    });
+
+
+
+    const sectionForm = document.querySelector('.section-form'),
+        formSection = sectionForm.querySelector('form'),
+        name = document.getElementById('name_2'),
+        phone = document.getElementById('phone_2');
+    name.addEventListener('input', () => {
+        name.value = name.value.replace(/[^а-я]/ig, '');
+    });
+    phone.addEventListener('input', () => {
+        phone.value = phone.value.replace(/[^+\d]/g, '');
+    });
+    formSection.addEventListener('submit', event => {
+        event.preventDefault();
+        formSection.appendChild(statusMessage);
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+            statusMessage.textContent = loadMessage;
+            if(request.readyState !== 4) {
+                return;
+            }
+            if(request.status === 200) {
+                statusMessage.textContent = successMessage;
+            } else {
+                statusMessage.textContent = errorMessage;
+            }
+        });
+        request.open('POST', './server.php');
+        request.setRequestHeader('Content-Type', 'application/json');
+        const formData = new FormData(formSection);
+        let body = {};
+        formData.forEach((value, key) => {
+            body[key] = value;
+        });
+        request.send(JSON.stringify(body));
+    });
 
 };
 
